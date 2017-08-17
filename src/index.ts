4ac25b663e40e41c28992d26c20f8bdc27a9bfb8
Ryan <untuned20@gmail.com>
2017-08-17 12:06:33 -0500

    Get the default require

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-///<reference path="../typings/index.d.ts" />
 import * as path from 'path'
 
 import { run } from 'spikit'
 
 // Set the root of the project
 // This path contains the public, resources and app directories
+let projectRoot = path.resolve(__dirname, '..')
+let appRoot = path.resolve(__dirname)
 
-global.projectRoot = path.resolve(__dirname, '/..')
-global.appRoot = path.resolve(__dirname)
-
-run()
+run(projectRoot, appRoot)
