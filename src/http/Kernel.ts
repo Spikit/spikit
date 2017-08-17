73a624c1d3bed2f3ad44dc1c5c7c7d3fe3799e49
Ryan <untuned20@gmail.com>
2017-08-17 12:00:37 -0500

    Get the default require

--- src/http/Kernel.ts
+++ src/http/Kernel.ts
@@ -1,12 +1,39 @@
-import { Locale } from 'spikit'
+import { Locale, Auth } from 'spikit'
 
 export default class Kernel {
-  protected middleware = [
+  /**
+   *----------------------------------------------------------------------------
+   * Middleware
+   *----------------------------------------------------------------------------
+   *
+   * The application's global HTTP middleware stack.
+   *
+   * The middleware listed here is run during every request to your application.
+   */
+  public static readonly middleware = [
     Locale
   ]
 
-  protected middlewareGroups = {
+  /**
+   *----------------------------------------------------------------------------
+   * Middleware Groups
+   *----------------------------------------------------------------------------
+   *
+   * This is a list of groups that only run on defined middleware groups
+   */
+  public static readonly middlewareGroups = {
     web: [],
     api: []
+  }
+
+  /**
+   *----------------------------------------------------------------------------
+   * Middleware Groups
+   *----------------------------------------------------------------------------
+   *
+   * This is a list of groups that only run on defined middleware groups
+   */
+  public static readonly routeMiddleware = {
+    auth: Auth
   }
 }
