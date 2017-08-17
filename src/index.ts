194eb444b665eecc3818a5a1b83cfac7daedabe0
Ryan <untuned20@gmail.com>
2017-08-17 11:01:01 -0500

    Moved logic from spikit to spikitlib

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
-import * as glob from 'glob'
+import * as path from 'path'
 
-import { init, start } from 'spikit'
-import view from './config/view'
-import app from './config/app'
-import server from './config/server'
-import sass from './config/sass'
+import { run } from 'spikit'
 
-import Kernel from './http/Kernel'
+// Set the root of the project
+// This path contains the public, resources and app directories
+let projectRoot = path.resolve(__dirname, '/..')
+let appRoot = path.resolve(__dirname)
 
-init({
-  app: app,
-  view: view,
-  static: app.static,
-  server: server,
-  sass: sass,
-  kernel: Kernel
-})
-
-// Load an import our routes
-glob(__dirname + '/routes/**/*.js', (err, files: string[]) => {
-  files.forEach(file => require(file))
-  start()
-})
+run(projectRoot, appRoot)
