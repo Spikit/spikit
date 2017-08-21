12c4b08ed48a3368dfa2d16fc6635ffce14a2978
Ryan Naddy <rnaddy@corp.acesse.com>
2017-08-21 17:36:14 -0500

    added comments to index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+/**
+ *----------------------------------------------------------------------------
+ * DO NOT MODIFY
+ *----------------------------------------------------------------------------
+ *
+ * You should not modify this file unless you know what you are doing.
+ * Modifying this file can break the initialization and execution of the library.
+ */
 import * as path from 'path'
 
+// Import the spikit initializer.
+// This will initialize spikit and start the spikit server.
 import { run } from 'spikit'
 
-// Set the root of the project
-// This path contains the public, resources and app directories
-let projectRoot = path.resolve(__dirname, '..')
-let appRoot = path.resolve(__dirname)
-
-run(projectRoot, appRoot)
+// Set the root of the project and the project app
+run(path.resolve(__dirname, '..'), path.resolve(__dirname))
