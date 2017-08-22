94a4c9b92f850af4988271b01ac2cc4b4bd65164
Ryan Naddy <untuned20@gmail.com>
2017-08-21 20:59:53 -0500

    Create typescript.ts

---
+++ src/config/typescript.ts
@@ -0,0 +1,11 @@
+import * as path from 'path'
+
+export default {
+  enabled: true,
+  tsc: {
+    sourceRoot: path.join(process.cwd(), 'resources/typescript'),
+    outDir: path.join(process.cwd(), 'public/js'),
+    target: 'es6',
+    module: 'system'
+  }
+}
